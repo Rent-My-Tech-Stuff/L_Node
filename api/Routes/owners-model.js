e43b4d4bf4b6fc51f3a2a60173efbd891960a232
de_gentlem4n <th3g3n3lm4n@gmail.com>
2021-04-01 21:51:46 -0400

    :disappointed: mini model setup needs work...

---
+++ api/Routes/owners-model.js
@@ -0,0 +1,22 @@
+const db = require('db')
+
+function getItembyItemId(itemId){
+
+} // returns item by item id
+
+
+function getItemsByOwnerId(ownerId){
+
+} // returns items by owner id 
+
+function addItem(item){
+
+} // adds items 
+
+function editItem(item){
+
+} //edits item 
+
+function removeItem(id){
+
+} // removies item 
---
+++ api/Routes/users-model.js
@@ -0,0 +1,15 @@
+const db = require('../data/db-config')
+
+
+function addUser(user){
+  
+} //add user function
+
+function findById(id){
+
+}// find users by id 
+
+module.exports = {
+  addUser,
+  findById
+}
